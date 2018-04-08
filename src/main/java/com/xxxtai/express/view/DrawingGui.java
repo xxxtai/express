40a6930aa06cfa7a5ca2a96ab6bc1589f3d6c17e
zhongtai <xxxtai@163.com>
2018-04-08 16:20:04 +0800

    delete drawing gui

--- src/main/java/com/xxxtai/express/view/DrawingGui.java
+++ src/main/java/com/xxxtai/express/view/DrawingGui.java
@@ -1,8 +1,10 @@
 package com.xxxtai.express.view;
 
 import com.xxxtai.express.toolKit.Common;
+import com.xxxtai.express.toolKit.MyTextField;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -13,24 +15,43 @@ public class DrawingGui extends JPanel{
     private RoundButton schedulingGuiBtn;
     private RoundButton settingGuiBtn;
     private RoundButton drawingGuiBtn;
+    private RoundButton importGraphBtn;
+    private RoundButton reflectGraphBtn;
+    private MyTextField rowField;
+    private MyTextField columnField;
+    private MyTextField realDistanceField;
+    private RoundButton confirmAddExitBtn;
 
     public DrawingGui() {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        schedulingGuiBtn = new RoundButton("监控界面");
+        schedulingGuiBtn = new RoundButton("调度界面");
         schedulingGuiBtn.setBounds(0, 0, screenSize.width / 3, screenSize.height / 20);
 
         settingGuiBtn = new RoundButton("设置界面");
         settingGuiBtn.setBounds(screenSize.width / 3, 0, screenSize.width / 3, screenSize.height / 20);
 
-        drawingGuiBtn = new RoundButton("管理界面");
+        drawingGuiBtn = new RoundButton("制图界面");
         drawingGuiBtn.setBounds(2 * screenSize.width / 3, 0, screenSize.width / 3, screenSize.height / 20);
         drawingGuiBtn.setForeground(new Color(30, 144, 255));
         drawingGuiBtn.setBackground(Color.WHITE);
+
+        rowField = new MyTextField("        行数");
+        rowField.setBounds(5 * screenSize.width / 12, 3 * screenSize.height / 15, screenSize.width / 6, screenSize.height / 20);
+        columnField = new MyTextField("        列数");
+        columnField.setBounds(5 * screenSize.width / 12, 4 * screenSize.height / 15, screenSize.width / 6, screenSize.height / 20);
+        realDistanceField = new MyTextField("        距离");
+        realDistanceField.setBounds(5 * screenSize.width / 12, 5 * screenSize.height / 15, screenSize.width / 6, screenSize.height / 20);
 
         this.setLayout(null);
         this.add(schedulingGuiBtn);
         this.add(settingGuiBtn);
         this.add(drawingGuiBtn);
+        this.add(reflectGraphBtn);
+        this.add(columnField);
+        this.add(realDistanceField);
+        this.add(rowField);
+        this.add(importGraphBtn);
+        this.add(confirmAddExitBtn);
     }
 
     @Override
